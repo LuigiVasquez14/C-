1a925fec4789a3aec9347ef47bbeabae429efb31
Luigi <luigivsqz4@gmail.com>
2025-09-03 11:00:44 -0500

    Add files via upload

---
+++ 2.Gestion De Empleados 2.0/Gestion De Empleados 2.0/FrmFiltros.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Gestion_De_Empleados_2._0
+{
+    partial class FrmFiltros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            textBox1 = new TextBox();
+            comboBox1 = new ComboBox();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            dgConsultas = new DataGridView();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgConsultas).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Location = new Point(68, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(625, 249);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(comboBox1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(617, 221);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Nombre";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(394, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(148, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(51, 6);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(143, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(617, 221);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Genero";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(617, 221);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Cargo";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(617, 221);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Salario";
+            tabPage4.UseVisualStyleBackColor = true;
+            tabPage4.Click += tabPage4_Click;
+            // 
+            // dgConsultas
+            // 
+            dgConsultas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgConsultas.Location = new Point(72, 267);
+            dgConsultas.Name = "dgConsultas";
+            dgConsultas.Size = new Size(621, 150);
+            dgConsultas.TabIndex = 1;
+            // 
+            // FrmFiltros
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(67, 61, 105);
+            ClientSize = new Size(821, 548);
+            Controls.Add(dgConsultas);
+            Controls.Add(tabControl1);
+            Name = "FrmFiltros";
+            Text = "FrmFiltros";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgConsultas).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TextBox textBox1;
+        private ComboBox comboBox1;
+        private DataGridView dgConsultas;
+    }
+}
---
+++ 2.Gestion De Empleados 2.0/Gestion De Empleados 2.0/FrmFiltros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_De_Empleados_2._0
+{
+    public partial class FrmFiltros : Form
+    {
+        public FrmFiltros()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

941fb4d2f094d4f8dacd042230df84f81dc6183e
Luigi <luigivsqz4@gmail.com>
2025-09-26 10:43:24 -0500

    Add files via upload

---
+++ Concesionario Clases/Concesionario/DTO/VehiculoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concesionario.DTO
+{
+    public class VehiculoDTO
+    {
+        public string Placa { get; set; }
+        public string Color { get; set; } = "";
+
+        [DisplayName("Año de producción")]
+        public int AnioProduccion { get; set; } = 0;
+        public string Modelo { get; set; } = "";
+        public String Marca { get; set; } = "";
+        public string Propietario { get; set; } = "";
+    }
+}
